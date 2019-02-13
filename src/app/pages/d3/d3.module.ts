0cd7d50d107b0208bada22f8561de3be1bc72b5e
ayan sil <ayansil.com@gmail.com>
2019-02-13 13:03:48 +0530

    first commit

--- src/app/pages/d3/d3.module.ts
+++ src/app/pages/d3/d3.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { ThemeModule } from '../../@theme/theme.module';
 import { D3RoutingModule, routedComponents } from './d3-routing.module';
-import { AngularD3TreeLibModule } from 'angular-d3-tree';
+
 
 @NgModule({
   imports: [
     ThemeModule,
     D3RoutingModule,
     Ng2SmartTableModule,
-    AngularD3TreeLibModule,
   ],
   declarations: [
     ...routedComponents,
